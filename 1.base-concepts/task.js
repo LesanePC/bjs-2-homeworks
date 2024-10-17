fa9f1ed95e34ead8496bf076cb1badaf528af3a4
Eugene Fokin <evgen94@bk.ru>
2024-10-17 16:15:16 +0300

    first task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,32 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let x1;
+	let x2;
+	let d = b ** 2 - 4 * a * c;
+
+	if (d > 0) {
+		x1 = (-b + Math.sqrt(d)) / (2 * a);
+		x2 = (-b - Math.sqrt(d)) / (2 * a);
+		arr.push(x1, x2);
+	} else if (d === 0) {
+		x1 = -b / (2 * a);
+		arr.push(x1);
+	}
+	return arr;
 }
 
+console.log(solveEquation());
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let monthlyPercent = percent / 100 / 12;
+	let loanAmount = amount - contribution;
+	let monthlyPayment = loanAmount * (monthlyPercent + monthlyPercent / (((1 + monthlyPercent) ** countMonths) - 1));
+	let totalPayment = monthlyPayment * countMonths;
+	totalPayment = Number(totalPayment.toFixed(2));
+
+	return totalPayment;
+
+
 }
